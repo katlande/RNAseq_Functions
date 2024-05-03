db8d0977d68c823ca3b19eef7c3bc24689b67dac
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-05-03 15:27:01 -0700

    Update CheckDepth.R

--- R/CheckDepth.R
+++ R/CheckDepth.R
@@ -1,5 +1,5 @@
 CheckDepth <- function(raw, minDepth=1e07){
-  ggplot2::gplot(data.frame(nReads=colSums(raw), x="a"), ggplot2::aes(y=nReads, x=x))+
+  ggplot2::ggplot(data.frame(nReads=colSums(raw), x="a"), ggplot2::aes(y=nReads, x=x))+
     ggplot2::geom_boxplot()+ ggplot2::geom_jitter(width = 0.2, height = 0)+
     ggplot2::theme(axis.text.x = ggplot2::element_blank(), axis.ticks.x = ggplot2::element_blank(),
           axis.title.x = ggplot2::element_blank())+ ggplot2::ylab("Total Reads per Sample")+
