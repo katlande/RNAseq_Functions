b11677eb6c387db6108d86b9b86d6aab4132e080
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-05-07 11:34:15 -0700

    Add files via upload

--- R/SetScores.R
+++ R/SetScores.R
@@ -1,29 +1,48 @@
-SetScores <- function(norm, goi, meta){
-
-  tdf <- t(apply(norm, 1, FUN=function(x){as.numeric(scale(x))}))
-  row.names(tdf) <- row.names(norm)
-  colnames(tdf) <- colnames(norm)
-
-  if(is.list(goi)){
-    for(i in 1:length(goi)){
-
-      if(i == 1){
-        goidf <- data.frame(Sample=colnames(tdf),
-                            Score=rowMedians(t(tdf[row.names(tdf)%in%goi[[i]],])),
-                            Set=names(goi)[i])
-      } else{
-        tmp <- data.frame(Sample=colnames(tdf),
-                            Score=rowMedians(t(tdf[row.names(tdf)%in%goi[[i]],])),
-                            Set=names(goi)[i])
-        goidf <- rbind(goidf, tmp)
+SetScores <- function (norm, goi, meta, out="data.frame") 
+{
+  
+  if(!out%in%c("data.frame", "matrix")){
+    warning(paste(out, "is not a recognized option for variable 'out'; must be one of 'data.frame' or 'matrix'"))
+  } else {
+    
+    tdf <- t(apply(norm, 1, FUN = function(x) {
+      as.numeric(scale(x))
+    }))
+    row.names(tdf) <- row.names(norm)
+    colnames(tdf) <- colnames(norm)
+    if (is.list(goi)) {
+      for (i in 1:length(goi)) {
+        if (i == 1) {
+          
+          if(out == "data.frame"){
+            goidf <- data.frame(Sample = colnames(tdf), 
+                                Score = rowMedians(t(tdf[row.names(tdf) %in% 
+                                                           goi[[i]], ])), Set = names(goi)[i])
+          } else{
+            goidf <- data.frame(Sample = colnames(tdf), 
+                                Set = rowMedians(t(tdf[row.names(tdf) %in% goi[[i]], ])))
+            colnames(goidf)[2] <- names(goi)[i]
+          }
+        }
+        else {
+          if(out == "data.frame"){
+            tmp <- data.frame(Sample = colnames(tdf), Score = rowMedians(t(tdf[row.names(tdf) %in% 
+                                                                                 goi[[i]], ])), Set = names(goi)[i])
+            goidf <- rbind(goidf, tmp)
+          } else{
+            tmp <- data.frame(Sample = colnames(tdf), 
+                              Set = rowMedians(t(tdf[row.names(tdf) %in% goi[[i]], ])))
+            colnames(tmp)[2] <- names(goi)[i]
+            goidf <- merge(goidf, tmp, by="Sample")
+          }
+        }
       }
     }
-  } else{
-    goidf <- data.frame(Sample=colnames(tdf), Score=rowMedians(t(tdf[row.names(tdf)%in%goi,])))
+    else {
+      goidf <- data.frame(Sample = colnames(tdf), Score = rowMedians(t(tdf[row.names(tdf) %in% goi, ])))
+    }
+    score_df <- meta %>% rownames_to_column("Sample") %>% merge(., 
+                                                                goidf, by = "Sample", all.x = F, all.y = T)
+    return(score_df)
   }
-
-  meta %>% rownames_to_column("Sample") %>%
-    merge(. , goidf, by="Sample", all.x=F, all.y=T) -> score_df
-
-  return(score_df)
 }
