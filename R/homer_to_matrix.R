59317292e6c365ca9a7ad8a9279b96092b4807f9
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-06-06 11:07:35 -0700

    Update homer_to_matrix.R

--- R/homer_to_matrix.R
+++ R/homer_to_matrix.R
@@ -1,19 +1,30 @@
-homer_to_matrix <- function(raw, cutchar = "_", instance="1", pool=FALSE){
+homer_to_matrix <- function (raw, cutchar = "_", instance = "1", pool = FALSE, round=TRUE) 
+{
   raw <- raw[c(8:ncol(raw))]
   colnames(raw)[c(1)] <- c("Gene")
-  if(instance==1){
+  if (instance == 1) {
     colnames(raw) <- gsub(paste0(cutchar, ".*"), "", colnames(raw))
-  } else{
-    colnames(raw) <- sub(sprintf(paste0("^((.*?", cutchar, "){%d}.*?)", cutchar, ".*"), (instance-1)), "\\1", colnames(raw))
+  }
+  else {
+    colnames(raw) <- sub(sprintf(paste0("^((.*?", cutchar, 
+                                        "){%d}.*?)", cutchar, ".*"), (instance - 1)), "\\1", 
+                         colnames(raw))
   }
   raw$Gene <- sub("[|][|].*", "", raw$Gene)
   raw$Gene <- sub("[|].*", "", raw$Gene)
-  if(pool==T){
-    raw <- aggregate(raw[c(2:ncol(raw))], by=list(raw$Gene), FUN="sum")
+  if (pool == T) {
+    raw <- aggregate(raw[c(2:ncol(raw))], by = list(raw$Gene), 
+                     FUN = "sum")
     colnames(raw)[c(1)] <- c("Gene")
   }
-
   row.names(raw) <- raw$Gene
   raw <- raw[-c(1)]
+  
+  if(round==T){
+    r <- as.data.frame(apply(raw, 2, as.integer))
+    row.names(r) <- row.names(raw)
+    raw <- r
+  }
+  
   return(raw)
 }
