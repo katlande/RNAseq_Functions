fd6791f13d07726e79c2cd5493a8c07c4147eac3
Kathryn Lande <46092714+katlande@users.noreply.github.com>
2024-12-16 12:38:52 -0800

    Update writeTSV.R

--- R/writeTSV.R
+++ R/writeTSV.R
@@ -1,4 +1,4 @@
 writeTSV <- function(df, dir, rows=F){
-  write.table(df, dir, quote=F, col.names=T, row.names==rows, sep="\t")
+  write.table(df, paste0(dir,".tsv"), quote=F, col.names=T, row.names==rows, sep="\t")
 }
    
